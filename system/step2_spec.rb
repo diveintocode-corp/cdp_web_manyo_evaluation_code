fb870a3387e91833a71876e58d34f161ee447e5f
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-02-04 11:33:49 +0900

    step2を翻訳

--- system/step2_spec.rb
+++ system/step2_spec.rb
@@ -2,118 +2,118 @@ require 'rails_helper'
 
 RSpec.describe 'step2', type: :system do
 
-  describe '開発要件' do
+  describe 'development requirements' do
     let!(:task) { Task.create(title: 'task_title', content: 'task_content') }
-    describe 'i18nを使って要件通りに文字やリンク、ボタンを国際化すること' do
-      it 'グローバルナビゲーション' do
+    describe 'Internationalize text, links and buttons as per requirements using i18n' do
+      it 'Global navigation' do
         visit root_path
-        expect(page).to have_selector '#tasks-index', text: 'タスク一覧'
-        expect(page).to have_selector '#new-task', text: 'タスクを登録する'
+        expect(page).to have_selector '#tasks-index', text: 'List of tasks'
+        expect(page).to have_selector '#new-task', text: 'Register a task'
       end
-      it 'タスク一覧画面' do
+      it 'task list screen' do
         visit tasks_path
-        expect(page).to have_content 'タスク一覧ページ'
-        expect(page).to have_selector '.show-task', text: '詳細'
-        expect(page).to have_selector '.edit-task', text: '編集'
-        expect(page).to have_selector '.destroy-task', text: '削除'
+        expect(page).to have_content 'Task List Page'
+        expect(page).to have_selector '.show-task', text: 'Details'
+        expect(page).to have_selector '.edit-task', text: 'edit'
+        expect(page).to have_selector '.destroy-task', text: 'delete'
         find('.destroy-task').click
-        expect(page.driver.browser.switch_to.alert.text).to eq '本当に削除してもよろしいですか？'
+        expect(page.driver.browser.switch_to.alert.text).to eq 'Are you sure you want to delete this?'
       end
-      it 'タスク登録画面' do
+      it 'task registration screen' do
         visit new_task_path
-        expect(page).to have_content 'タスク登録ページ'
-        expect(page).to have_selector 'label', text: 'タイトル'
-        expect(page).to have_selector 'label', text: '内容'
-        expect(page).to have_button '登録する'
-        expect(page).to have_selector '#back', text: '戻る'
+        expect(page).to have_content 'Task registration page'
+        expect(page).to have_selector 'label', text: 'Title'
+        expect(page).to have_selector 'label', text: 'content'
+        expect(page).to have_button 'Register'
+        expect(page).to have_selector '#back', text: 'Back'
       end
-      it 'タスク詳細画面' do
+      it 'task details screen' do
         visit task_path(task)
-        expect(page).to have_content 'タスク詳細ページ'
-        expect(page).to have_content 'タイトル'
-        expect(page).to have_content '内容'
-        expect(page).to have_selector '#edit-task', text: '編集'
-        expect(page).to have_selector '#back', text: '戻る'
+        expect(page).to have_content 'Task detail page'
+        expect(page).to have_content 'Title'
+        expect(page).to have_content 'Content'
+        expect(page).to have_selector '#edit-task', text: 'edit'
+        expect(page).to have_selector '#back', text: 'Back'
       end
-      it 'タスク編集画面' do
+      it 'task edit screen' do
         visit edit_task_path(task)
-        expect(page).to have_content 'タスク編集ページ'
-        expect(page).to have_content 'タイトル'
-        expect(page).to have_content '内容'
-        expect(page).to have_button '更新する'
-        expect(page).to have_selector '#back', text: '戻る'
+        expect(page).to have_content 'Edit Task Page'
+        expect(page).to have_content 'Title'
+        expect(page).to have_content 'Content'
+        expect(page).to have_button 'Update'
+        expect(page).to have_selector '#back', text: 'Back'
       end
-      context 'バリデーションメッセージ' do
-        it 'タスク登録画面でタイトルと内容が未入力の場合' do
+      context 'Validation message' do
+        it 'If the title and content are not entered in the task registration screen' do
           visit new_task_path
           find('input[name="task[title]"]').set('')
           find('textarea[name="task[content]"]').set('')
           find('#create-task').click
-          expect(page).to have_content "タイトルを入力してください"
-          expect(page).to have_content "内容を入力してください"
+          expect(page).to have_content "Please enter a title."
+          expect(page).to have_content "Please enter the content"
         end
-        it 'タスク編集画面でタイトルと内容が未入力の場合' do
+        it 'If the title and content are not entered in the task edit screen' do
           visit edit_task_path(task)
           find('input[name="task[title]"]').set('')
           find('textarea[name="task[content]"]').set('')
           find('#update-task').click
-          expect(page).to have_content "タイトルを入力してください"
-          expect(page).to have_content "内容を入力してください"
+          expect(page).to have_content "Please enter a title"
+          expect(page).to have_content "Please enter the content"
         end
       end
-      context 'フラッシュメッセージ' do
-        it 'タスクの登録に成功した場合' do
+      context 'Flash message' do
+        it 'If the task was successfully registered' do
           visit new_task_path
-          fill_in 'タイトル', with: 'sample title'
-          fill_in '内容', with: 'sample content'
-          click_button '登録する'
-          expect(page).to have_content "タスクを登録しました"
+          fill_in 'title', with: 'sample title'
+          fill_in 'content', with: 'sample content'
+          click_button 'register'
+          expect(page).to have_content "You have registered a task."
         end
-        it 'タスクの更新に成功した場合' do
+        it 'If the task was successfully updated' do
           visit edit_task_path(task)
-          fill_in 'タイトル', with: 'update sample title'
-          fill_in '内容', with: 'update sample content'
-          click_button '更新する'
-          expect(page).to have_content "タスクを更新しました"
+          fill_in 'title', with: 'update sample title'
+          fill_in 'content', with: 'update sample content'
+          click_button 'update'
+          expect(page).to have_content "You have updated the task."
         end
-        it 'タスクを削除した場合' do
+        it 'Deleting a task' do
           visit tasks_path
-          click_link '削除'
+          click_link 'delete'
           page.driver.browser.switch_to.alert.accept
-          expect(page).to have_content "タスクを削除しました"
+          expect(page).to have_content "Deleted the task."
         end
       end
     end
-    describe 'タスク一覧画面に表示させる作成日時を、あなたの住んでいる地域の時刻に設定すること' do
-      it '作成日時に協定世界時を意味する「UTC」の文字が表示されていないこと' do
+    describe 'Set the creation date and time to be displayed in the task list screen to the time in your region' do
+      it 'The creation date and time should not have the characters "UTC" which means Coordinated Universal Time.' do
         visit tasks_path
-        expect(page).not_to have_content "UTC"
+        expect(page).not_to_have_content "UTC"
       end
     end
-    describe 'データベースのデータを読み書きする際の時刻を、あなたの住んでいる地域の時刻に設定すること' do
-      it 'タスクのcreated_atカラムのデータを出力させた際、「+0900」が表示されること' do
+    describe 'Setting the time for reading and writing data in the database to the time in your region' do
+      it 'When outputting the data in the created_at column of the task, "+0900" should be displayed' do
         task = Task.create(title: 'task_title', content: 'task_content')
         expect(task.created_at.to_s).to include('+0900')
       end
     end
-    describe 'seedデータを使って、50件分のタスクデータを投入できるようにすること' do
+    describe 'To be able to submit task data for 50 tasks using seed data' do
       before do
         load Rails.root.join("db/seeds.rb")
       end
-      it 'seedデータを使って、50件分のタスクデータを投入できるようにすること' do
+      it 'To be able to use seed data to populate 50 tasks' do
         expect(Task.all.count).to be >= 50
       end
     end
   end
 
-  describe '機能要件' do
+  describe 'functional requirements' do
     before do
       50.times do |n|
         Task.create(title: "task_title_#{n+1}", content: "task_content_#{n+1}")
       end
     end
-    describe 'タスク一覧画面でタスクを作成日時の降順で表示させること' do
-      it 'タスク一覧画面でタスクを作成日時の降順で表示させること' do
+    describe 'Display tasks in the task list screen in descending order by creation date and time' do
+      it 'To display tasks in descending order of creation date in the task list screen' do
         visit tasks_path
         tasks = all('tbody tr')
         expect(tasks[0].text).to include('task_title_50')
@@ -128,12 +128,12 @@ RSpec.describe 'step2', type: :system do
         expect(tasks[9].text).to include('task_title_41')
       end
     end
-    describe 'kaminariのgemを使って、タスク一覧画面にページネーションを実装し、1ページあたり10件のタスクを表示させること' do
-      it 'タスク一覧画面にkaminariを適用させた際に作られるpaginationクラスが存在すること' do
+    describe "Using kaminari's gem, implement pagination in the task list screen to display 10 tasks per page" do
+      it 'There should be a pagination class created when kaminari is applied to the task list screen' do
         visit tasks_path
         expect(page).to have_css '.pagination'
       end
-      it 'タスクに付与しているHTMLのクラス属性（show-task、edit-task、destroy-task）が10件分表示されること' do
+      it 'The HTML class attributes (show-task, edit-task, destroy-task) assigned to the tasks should be displayed for 10 tasks' do
         visit tasks_path
         expect(all('.show-task').count()).to eq 10
         expect(all('.edit-task').count()).to eq 10
